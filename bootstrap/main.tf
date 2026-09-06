8738ae2eeb256182c046b3634288a73247627c54
Kevin Truong <165167213+kevtruo@users.noreply.github.com>
2026-09-05 23:48:57 -0400

    feat: add bootstrap/main.tf

---
+++ bootstrap/main.tf
@@ -0,0 +1,159 @@
+provider "aws" {
+  region = var.aws_region
+
+  default_tags {
+    tags = {
+      Project   = "aws-iac-pipeline-showcase"
+      ManagedBy = "Terraform"
+    }
+  }
+}
+
+data "aws_caller_identity" "current" {}
+
+data "tls_certificate" "github" {
+  url = "https://token.actions.githubusercontent.com"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "${var.name_prefix}-tfstate-${data.aws_caller_identity.current.account_id}"
+}
+
+resource "aws_s3_bucket_versioning" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "terraform_state" {
+  bucket                  = aws_s3_bucket.terraform_state.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "${var.name_prefix}-terraform-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+resource "aws_iam_openid_connect_provider" "github" {
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [data.tls_certificate.github.certificates[0].sha1_fingerprint]
+}
+
+data "aws_iam_policy_document" "plan_trust" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+
+    principals {
+      type        = "Federated"
+      identifiers = [aws_iam_openid_connect_provider.github.arn]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:sub"
+      values   = ["repo:${var.github_owner}/${var.github_repository}:pull_request"]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "apply_trust" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+
+    principals {
+      type        = "Federated"
+      identifiers = [aws_iam_openid_connect_provider.github.arn]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:sub"
+      values   = ["repo:${var.github_owner}/${var.github_repository}:environment:${var.apply_environment}"]
+    }
+  }
+}
+
+resource "aws_iam_role" "terraform_plan" {
+  name               = "${var.name_prefix}-terraform-plan"
+  assume_role_policy = data.aws_iam_policy_document.plan_trust.json
+}
+
+resource "aws_iam_role_policy" "terraform_plan" {
+  name = "read-example-resources"
+  role = aws_iam_role.terraform_plan.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Action = [
+        "s3:GetBucketLocation",
+        "s3:GetBucketTagging",
+        "s3:GetBucketVersioning",
+        "s3:ListBucket"
+      ]
+      Resource = "arn:aws:s3:::example-*"
+    }]
+  })
+}
+
+resource "aws_iam_role" "terraform_apply" {
+  name               = "${var.name_prefix}-terraform-apply"
+  assume_role_policy = data.aws_iam_policy_document.apply_trust.json
+}
+
+resource "aws_iam_role_policy" "terraform_apply" {
+  name = "manage-example-resources"
+  role = aws_iam_role.terraform_apply.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Action = [
+        "s3:CreateBucket",
+        "s3:DeleteBucket",
+        "s3:Get*",
+        "s3:List*",
+        "s3:PutBucketTagging",
+        "s3:PutBucketVersioning"
+      ]
+      Resource = "arn:aws:s3:::example-*"
+    }]
+  })
+}
